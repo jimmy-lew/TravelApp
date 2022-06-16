60d34f405cae79d125dc66011caab7f6f053963e
Addison <S10222525@connect.np.edu.sg>
2022-06-17 04:44:56 +0800

    Init Bus Class - Added relevant attributes to be stored from API

---
+++ app/src/main/java/sg/edu/np/mad/travelapp/Bus.java
@@ -0,0 +1,75 @@
+package sg.edu.np.mad.travelapp;
+
+public class Bus {
+    // Documentation : https://datamall.lta.gov.sg/content/dam/datamall/datasets/LTA_DataMall_API_User_Guide.pdf#page=12&zoom=100,92,134
+
+    public int ServiceNo;
+
+    public String Type;         // Wheelchair accessible?
+    public String BusType;
+    public String Load;         // How crowded the bus is | (SEA(for Seats Available) ▪ SDA (for Standing Available)▪ LSD (for Limited Standing)
+
+    public double Latitude;
+    public double Longitude;
+
+
+    // ---- Getters & Setters ----
+    public int getServiceNo() {
+        return ServiceNo;
+    }
+
+    public void setServiceNo(int serviceNo) {
+        ServiceNo = serviceNo;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public String getBusType() {
+        return BusType;
+    }
+
+    public void setBusType(String busType) {
+        BusType = busType;
+    }
+
+    public String getLoad() {
+        return Load;
+    }
+
+    public void setLoad(String load) {
+        Load = load;
+    }
+
+    public double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        Latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        Longitude = longitude;
+    }
+
+    // --- Constructors ---
+    public Bus(int serviceNo, String type, String busType, String load, double latitude, double longitude) {
+        ServiceNo = serviceNo;
+        Type = type;
+        BusType = busType;
+        Load = load;
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+}
