f77d93895b8521b9fd45156c5646a64e1423a0ec
Ryan Lim <s10192609@connect.np.edu.sg>
2022-06-17 01:47:16 +0800

    base xml of navbar

--- app/src/main/java/sg/edu/np/mad/travelapp/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/travelapp/MainActivity.java
@@ -2,6 +2,7 @@ package sg.edu.np.mad.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
