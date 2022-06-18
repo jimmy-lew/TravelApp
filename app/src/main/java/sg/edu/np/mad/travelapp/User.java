757a90e9a805b967744d5ef4a065b79de9dd3dc6
realelongatedmusket <s10222819@connect.np.edu.sg>
2022-06-18 23:51:31 +0800

    Added User Class

---
+++ app/src/main/java/sg/edu/np/mad/travelapp/User.java
@@ -0,0 +1,20 @@
+package sg.edu.np.mad.travelapp;
+
+public class User {
+    public String ID;
+
+    // Getter & Setter
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    // Constructor
+    public User(String ID) {
+        this.ID = ID;
+    }
+
+}
