481d3c27bf8b1e7f3105dc36ceaf8e80d126ce63
Lucas <defalt1765@gmail.com>
2022-06-19 10:17:24 +0800

    Implemented ViewFavourites page.

--- app/src/main/java/sg/edu/np/mad/travelapp/ViewFavourites.java
+++ app/src/main/java/sg/edu/np/mad/travelapp/ViewFavourites.java
@@ -2,13 +2,71 @@ package sg.edu.np.mad.travelapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ViewFavourites extends AppCompatActivity {
+    private final String TAG = "ViewFavouritespActivity";
+    private View decorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_favourites);
+
+        ImageView homeIcon = findViewById(R.id.homeIcon);
+        ImageView nearbyIcon = findViewById(R.id.nearbyIcon);
+        ImageView favIcon = findViewById(R.id.favIcon);
+
+        favIcon.setImageResource(R.drawable.favorite);
+
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent MainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(MainActivity);
+            }
+        });
+
+        nearbyIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ViewBusStops = new Intent(getApplicationContext(), ViewBusStops.class);
+                startActivity(ViewBusStops);
+            }
+        });
+
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(hideSystemBars());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        {
+            // If there is focus on the window, hide the status bar and navigation bar.
+            if (hasFocus) {
+                decorView.setSystemUiVisibility(hideSystemBars());
+            }
+        }
+    }
+
+    public int hideSystemBars() {
+        // Use Bitwise Operators to combine the flags
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
     }
 }
