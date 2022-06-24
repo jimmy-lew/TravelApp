54949a3c301a13f723be22a0babb91e21f040f1f
Lucas <defalt1765@gmail.com>
2022-06-24 11:49:48 +0800

    Created Navbar Fragment.

---
+++ app/src/main/java/sg/edu/np/mad/travelapp/NavbarFragment.java
@@ -0,0 +1,28 @@
+package sg.edu.np.mad.travelapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NavbarFragment extends Fragment {
+    public NavbarFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_navbar, container, false);
+    }
+}
