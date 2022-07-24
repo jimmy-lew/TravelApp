1839014a81675220f6a16ec43c855256af042b05
LucasHo <defalt1765@gmail.com>
2022-07-24 10:12:33 +0800

    Implemented Profile Analytics and Settings fragments.

---
+++ app/src/main/java/sg/edu/np/mad/travelapp/AnalyticsFragment.java
@@ -0,0 +1,31 @@
+package sg.edu.np.mad.travelapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AnalyticsFragment extends Fragment {
+
+    public AnalyticsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_analytics, container, false);
+    }
+}
