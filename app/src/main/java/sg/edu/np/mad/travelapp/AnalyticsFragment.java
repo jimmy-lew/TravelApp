f92c29a56708f3f83e78d509189392a2df75b318
LucasHo <defalt1765@gmail.com>
2022-07-24 18:35:01 +0800

    Implemented Analytics model class to store data for Analytics dashboard.

--- app/src/main/java/sg/edu/np/mad/travelapp/AnalyticsFragment.java
+++ app/src/main/java/sg/edu/np/mad/travelapp/AnalyticsFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class AnalyticsFragment extends Fragment {
-
     public AnalyticsFragment() {
         // Required empty public constructor
     }
@@ -23,8 +22,7 @@ public class AnalyticsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_analytics, container, false);
     }
