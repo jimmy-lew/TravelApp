285e3f60cc1f238fba9f8ab0cb835d5538601290
Addison <S10222525@connect.np.edu.sg>
2022-07-27 08:30:14 +0800

    Init: Fare Calculator Activity - Initialized activity_fare_calculator.xml - Add UI Elements & Design

---
+++ app/src/main/java/sg/edu/np/mad/travelapp/FareCalculator.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.travelapp;
+
+import android.os.Bundle;
+
+import sg.edu.np.mad.travelapp.ui.BaseActivity;
+
+public class FareCalculator extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fare_calculator);
+    }
+
+}
