23e74336d09de142eb3d11fd3ec9f9f2a7e12c00
Addison <S10222525@connect.np.edu.sg>
2022-07-30 07:12:17 +0800

    Refactor: CalculateBusFare Function - Refactored function - Add ConvertDistanceToRange function

--- app/src/main/java/sg/edu/np/mad/travelapp/FareCalculator.java
+++ app/src/main/java/sg/edu/np/mad/travelapp/FareCalculator.java
@@ -16,6 +16,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -70,23 +71,12 @@ public class FareCalculator extends BaseActivity {
     }
 
     // [ToDo] API Call to calculate fare
-    public double CalculateBusFare(int fareType, String from, String to, String busNo){
-        Log.v(TAG, "Calculating fare for " + busNo);
-        String tripInfo = "usiAccumulatedDistance1=0-usiAccumulatedDistance2=0-usiAccumulatedDistance3=0-usiAccumulatedDistance4=0-usiAccumulatedDistance5=0-usiAccumulatedDistance6=0-usiAccumulatedFare1=0-usiAccumulatedFare2=0-usiAccumulatedFare3=0-usiAccumulatedFare4=0-usiAccumulatedFare5=0-usiAccumulatedFare6=0";
-        String url = "https://www.lta.gov.sg/content/ltagov/en/map/fare-calculator/jcr:content/map2-content/farecalculator.busget.html?fare="+ fareType + "&from=" + from + "&to=" + to + "&tripInfo=" + tripInfo + "&bus=" + busNo;
+    public double CalculateBusFare(ArrayList<String> fareType, double distance){
+        String url = "https://data.gov.sg/api/action/datastore_search?resource_id=cd2db3f7-bb5d-43d1-9264-955d71eeaf97";
+        // Tried using LTA's API (Fare Calculator) But their Bus Stop IDs are no the same as the Bus Stop Codes.
         OkHttpClient client = new OkHttpClient();
-
-        RequestBody formBody = new FormBody.Builder()
-                .add("fare=", String.valueOf(GetFareType()))
-                .add("from=", from)
-                .add("to=", to)
-                .add("tripInfo=", tripInfo)
-                .add("bus=", busNo)
-                .build();
-
         Request request = new Request.Builder()
                 .url(url)
-                .post(formBody)
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
@@ -101,15 +91,26 @@ public class FareCalculator extends BaseActivity {
                     throw new IOException("Unexpected code " + response);
                 }
                 String responseData = response.body().string();
-                Log.v(TAG, "Response: " + responseData);
+                // Log.v(TAG, "Response: " + responseData);
                 try {
-                    JSONObject jsonResponse = new JSONObject(responseData);
-                    jsonResponse.get("fare");
-                    Log.v(TAG, "Fare: " + jsonResponse.get("fare"));
-                    total += Double.parseDouble(jsonResponse.get("fare").toString());
+                    JSONArray records = new JSONObject(responseData).getJSONObject("result").getJSONArray("records");
+                    for (int i = 0; i < records.length(); i++) {
+                        JSONObject record = records.getJSONObject(i);
+                        Log.v(TAG, "Query: " + ConvertDistanceToRange(distance));
+                        Log.v(TAG, "Bool: " + record.getString("_id").equals(ConvertDistanceToRange(distance)));
+                        if (record.getString("_id").equals(ConvertDistanceToRange(distance))) {
+                            Log.v(TAG, "Distance: " + record.getString("distance"));
+                            total += Double.parseDouble(record.getString(fareType.get(0)))- Double.parseDouble(fareType.get(1));
+                        }
+                    }
+
+
+                    // Log.v(TAG, "Records + " );
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+
             }
         });
         return 0;
@@ -132,7 +133,6 @@ public class FareCalculator extends BaseActivity {
         Log.v(TAG, "Fare: " + fare);
     }
 
-
     // [ToDo] Call API & Calculate Cost
     public double CalculateMrtFare(int fareType, String from, String to){
         return 0;
@@ -154,17 +154,19 @@ public class FareCalculator extends BaseActivity {
         destination.setLat(1.4138053998770526);
         destination.setLng(103.8093035017651);
         RouteRepository.getInstance().getRoute(origin, destination, RouteList ->{
-            Log.v(TAG, "Route List: " + RouteList.size());
-            total = 0;
             for (Route route: RouteList){
+                // Reset Values
+                Log.v(TAG,"Total: " + total);
+                Log.v(TAG, "Route: " + "NEW ROUTE --------------- ");
+                From = "";
+                To = "";
+                total = 0;
                 for (Leg leg : route.getLegs()){
                     for (Step step : leg.getSteps()){
-                        Log.v(TAG, "[BUS]" + String.valueOf(step.getMode().equals("BUS")));
-                        Log.v(TAG, step.getMode());
+                        // Log.v(TAG, "[BUS]: " + String.valueOf(step.getMode().equals("BUS")));
                         if(step.getMode().equals("BUS")){
                             Log.v(TAG, "[Bus Details]: " + step.getDetails().getFrom() + "| to |" + step.getDetails().getTo() + "Bus No: " + step.getDetails().getLine().getName());
-                            ConvertStopNameToCode(step.getDetails().getFrom(), step.getDetails().getTo());
-                             CalculateBusFare(GetFareType(), From, To, step.getDetails().getLine().getName());
+                             CalculateBusFare(GetFareType(), Double.parseDouble(step.getDistance().split(" ")[0]));
                         }
                         if(step.getMode().equals("MRT")){
                             total += 1;
@@ -183,19 +185,71 @@ public class FareCalculator extends BaseActivity {
         return 0;
     }
 
-    public int GetFareType() {
+    public ArrayList<String> GetFareType() {
         // [ToDo] Get Fare Type from User Settings
+        // ----- Offsets -----
+        // (using LTA Calculator > (201) Kent Ridge Terminal > Blk 455 (2.9km) | Celementi Station A (3.2km)
+        //Student Offset : 30 cents
+        //Adult Offset: 60 cents
+        //Senior Citizen: 45 cents
+        //workfare_transport : 50 cents
+        //Disabilities: 45 cents
+
         String Type = "Student";
         switch (Type) {
             case "Adult":
-                return 30;
+                return new ArrayList<>(Arrays.asList("adult_card_fare_per_ride","60"));
             case "Student":
-                return 40;
-            case "Senior Citizen":
-                return 39;
-            case "Workfare Transport Concession Scheme":
-                return 38;
+                return new ArrayList<>(Arrays.asList("student_card_fare_per_ride","30"));
+            case "Senior":
+                return new ArrayList<>(Arrays.asList("senior_citizen_card_fare_per_ride","45"));
+            case "Workfare":
+                return new ArrayList<>(Arrays.asList("workfare_transport_concession_card_fare_per_ride","30"));
+            case "Disability":
+                return new ArrayList<>(Arrays.asList("persons_with_disabilities_card_fare_per_ride","45"));
         }
-        return 0;
+        return new ArrayList<>(Arrays.asList("Cash","0"));
+    }
+
+    public int ConvertDistanceToRange(double input){
+        if (input <= 3.2){ return 1;}
+        else if (input <= 4.2){ return 2;}
+        else if (input <= 5.2){ return 3;}
+        else if (input <= 6.2){ return 4;}
+        else if (input <= 7.2){ return 5;}
+        else if (input <= 8.2){ return 6;}
+        else if (input <= 9.2){ return 7;}
+        else if (input <= 10.2){ return 8;}
+        else if (input <= 11.2){ return 9;}
+        else if (input <= 12.2){ return 10;}
+        else if (input <= 13.2){ return 11;}
+        else if (input <= 14.2){ return 12;}
+        else if (input <= 15.2){ return 13;}
+        else if (input <= 16.2){ return 14;}
+        else if (input <= 17.2){ return 15;}
+        else if (input <= 18.2){ return 16;}
+        else if (input <= 19.2){ return 17;}
+        else if (input <= 20.2){ return 18;}
+        else if (input <= 21.2){ return 19;}
+        else if (input <= 22.2){ return 20;}
+        else if (input <= 23.2){ return 21;}
+        else if (input <= 24.2){ return 22;}
+        else if (input <= 25.2){ return 23;}
+        else if (input <= 26.2){ return 24;}
+        else if (input <= 27.2){ return 25;}
+        else if (input <= 28.2){ return 26;}
+        else if (input <= 29.2){ return 27;}
+        else if (input <= 30.2){ return 28;}
+        else if (input <= 31.2){ return 29;}
+        else if (input <= 32.2){ return 30;}
+        else if (input <= 33.2){ return 31;}
+        else if (input <= 34.2){ return 32;}
+        else if (input <= 35.2){ return 33;}
+        else if (input <= 36.2){ return 34;}
+        else if (input <= 37.2){ return 35;}
+        else if (input <= 38.2){ return 36;}
+        else if (input <= 39.2){ return 37;}
+        else if (input <= 40.2){ return 38;}
+        else {return 39;}
     }
 }
